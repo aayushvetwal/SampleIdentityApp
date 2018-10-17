b0a0f76a5a8564053747cfed75c9342799334cc5
Aayush Vetwal <avetwal@sivicsolutionsgroup.com>
2018-10-17 17:38:27 -0400

    Init

---
+++ SampleMongo/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleMongo.Models
+{
+    public class User : IUser<int>
+    {
+        public int Id => ()
+    
+        public string UserName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ SampleMongo/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Sample.Startup))]
+
+namespace Sample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            string conn = ConfigurationManager.ConnectionStrings["SampleDbConn"].ConnectionString;
+            app.Use
+
+        }
+    }
+}
